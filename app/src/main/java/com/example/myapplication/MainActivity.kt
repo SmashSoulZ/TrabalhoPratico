f74baa518c80e9d55f21872d80351eb2de93c4e7
jhenriquesantos <101994085+jhenriquesantos@users.noreply.github.com>
2023-03-17 17:07:49 +0000

    rogerio

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Alo Alo Teste
-        // O rogerio esteve aquii
+        // O rogerio esteve aquiii
     }
 }
