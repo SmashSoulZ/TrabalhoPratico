03f20273b8a2f66980af95fa531c7cfa34d638af
Edgar <edgarfernandes@ipvc.pt>
2023-04-02 23:26:26 +0100

    Pagina Perfil com o historico em baixo

--- app/src/main/java/com/example/myapplication/perfil.kt
+++ app/src/main/java/com/example/myapplication/perfil.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.adapter.LineAdapter
@@ -16,18 +17,26 @@ class perfil : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
 
+
         myList = ArrayList<registos>()
+
 
         for (i in 0 until 20){
             val randomLugar = Random.nextInt(0, 32)
             val randomDay = Random.nextInt(1, 31)
             val randomMonth = Random.nextInt(1, 12)
-            val randomHour = Random.nextInt(0, 24)
+            val randomHourE = Random.nextInt(0, 24)
+            val randomHourS = Random.nextInt(0, 24)
 
 
-
-            myList.add(registos("Data - $randomDay/$randomMonth","Hora - $randomHour", randomLugar))
+            myList.add(registos("Data - $randomDay/$randomMonth","Hora de Entrada: $randomHourE","Hora de Saida: $randomHourS" , "Lugar - $randomLugar"))
         }
+
+        val total=myList.size
+        val NTT = findViewById<TextView>(R.id.NTT)
+        NTT.text = "Nº Total de Estacionamentos: "+total.toString()
+
+
         recycler_view = findViewById<RecyclerView>(R.id.recView)
         recycler_view.adapter=LineAdapter(myList)
         recycler_view.layoutManager = LinearLayoutManager(this)
