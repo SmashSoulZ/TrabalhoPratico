35e28aa103ade312ccb717703db2e27043fb1196
Andre Ribeiro <andre.ribeiro@ipvc.pt>
2023-04-18 12:08:58 +0100

    Noticias

---
+++ app/src/main/java/com/example/myapplication/dataclasses/Noticias.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.dataclasses
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class Noticias : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_noticias)
+    }
+}
