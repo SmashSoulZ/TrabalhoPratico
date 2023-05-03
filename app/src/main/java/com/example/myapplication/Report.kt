8b9d44ea997a51c080c19b0e92972a1139d865a8
Dartzu <frui@ipvc.pt>
2023-05-03 23:28:23 +0100

    MenuAtualizado

--- app/src/main/java/com/example/myapplication/Report.kt
+++ app/src/main/java/com/example/myapplication/Report.kt
@@ -1,11 +1,48 @@
 package com.example.myapplication
 
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 
 class Report : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.nav_mapa -> {
+                Toast.makeText(this,"nav_mapa", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, parque::class.java))
+                true
+            }
+
+            R.id.nav_reportar -> {
+                Toast.makeText(this, "nav_reportar", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, Report::class.java))
+                true
+            }
+            R.id.nav_perfil -> {
+                Toast.makeText(this, "nav_perfil", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, perfil::class.java))
+                true
+            }
+
+            else -> {super.onOptionsItemSelected(item)}
+        }
     }
 }
