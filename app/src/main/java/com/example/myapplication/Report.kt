e4b9b57cf7e6388975792f8b85a56b15aac3c704
jhenriquesantos <101994085+jhenriquesantos@users.noreply.github.com>
2023-05-03 22:08:13 +0100

    Report BD

--- app/src/main/java/com/example/myapplication/Report.kt
+++ app/src/main/java/com/example/myapplication/Report.kt
@@ -2,10 +2,47 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Report : AppCompatActivity() {
+
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report)
+
+        db = FirebaseFirestore.getInstance()
+
+        val reportButton = findViewById<Button>(R.id.btnSubmit)
+        reportButton.setOnClickListener {
+            val email = findViewById<EditText>(R.id.email).text.toString().trim()
+            val report = findViewById<EditText>(R.id.report).text.toString().trim()
+
+            if (email.isNotEmpty() && report.isNotEmpty()) {
+                val userReport = hashMapOf(
+                    "email" to email,
+                    "report" to report
+                )
+
+                db.collection("Report")
+                    .add(userReport)
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Report enviado com sucesso!", Toast.LENGTH_SHORT).show()
+                        findViewById<EditText>(R.id.email).setText("")
+                        findViewById<EditText>(R.id.report).setText("")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.e("parque", "Erro ao enviar o report", e)
+                        Toast.makeText(this, "Erro ao enviar o report", Toast.LENGTH_SHORT).show()
+                    }
+            } else {
+                Toast.makeText(this, "Por favor, preencha todos os campos.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/parque.kt
+++ app/src/main/java/com/example/myapplication/parque.kt
@@ -68,7 +68,9 @@ class parque : AppCompatActivity() {
                             marginEnd = resources.getDimensionPixelSize(R.dimen.cell_spacing)
                         }
                         setOnClickListener {
-                            showParkingTime(document.id, parkingTime)
+                            if (!parked) {
+                                showParkingTime(document.id, parkingTime)
+                            }
                         }
                     }
 
@@ -93,6 +95,7 @@ class parque : AppCompatActivity() {
             .addOnFailureListener { exception ->
                 Log.w(TAG, "Error getting documents: ", exception)
             }
+
         val switch1 = dialog.findViewById<Switch>(R.id.switch1)
 
         switch1?.setOnCheckedChangeListener { _, isChecked ->
@@ -182,6 +185,7 @@ class parque : AppCompatActivity() {
                 }
             }
         }
+
     }
 
     private fun showParkingTime(parkingId: String, parkingTime: Int) {
